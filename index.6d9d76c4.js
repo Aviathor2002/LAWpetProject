3d06afae6acbd6ab3f97fb29c9232fb409dc0977
Aviathor2002 <109354604+Aviathor2002@users.noreply.github.com>
2024-03-11 20:51:23 +0000

    Deploying to gh-pages from @ Aviathor2002/LAWpetProject@206889e237243679a8770173bdf1ff9747bb0af5 🚀

---
+++ index.6d9d76c4.js
@@ -0,0 +1,2 @@
+var modalWindow=document.querySelector("[data-modal]"),openModal=document.querySelector("[data-modal-open]"),closeModal=document.querySelector("[data-modal-close]");function togglemodal(){modalWindow.classList.toggle("is-hidden")}openModal.addEventListener("click",togglemodal),closeModal.addEventListener("click",togglemodal);
+//# sourceMappingURL=index.6d9d76c4.js.map
